50c400c491460410e08e78e0c173039cd88b0bca
AppleKid7 <kvltd00d@gmail.com>
2021-02-07 20:14:28 -0500

    Adding movement, collision detection, score

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ggez::event::EventHandler;
 use ggez::graphics::Rect;
-use ggez::GameResult;
+use ggez::input::keyboard;
 
 const SCREEN_HEIGHT: f32 = 600.;
 const SCREEN_WIDTH: f32 = 600.;
@@ -8,8 +8,11 @@ const SCREEN_WIDTH: f32 = 600.;
 const X_OFFSET: f32 = 20.;
 const PADDLE_WIDTH: f32 = 12.;
 const PADDLE_HEIGHT: f32 = 75.;
+const PADDLE_SPEED: f32 = 8.0;
 
 const BALL_RADIUS: f32 = 10.;
+const MIN_VEL: f32 = 3.0;
+const MAX_VEL: f32 = 5.0;
 
 //We want to store all the data Pong needs here
 struct MainState {
@@ -27,6 +30,33 @@ struct Ball {
     vel: Vector,
 }
 
+impl Ball {
+    fn new() -> Self {
+        use rand::{thread_rng, Rng};
+
+        let mut rng = thread_rng();
+        let mut x_vel = rng.gen_range(MIN_VEL, MAX_VEL);
+        let mut y_vel = rng.gen_range(MIN_VEL, MAX_VEL);
+
+        if rng.gen::<bool>() {
+            x_vel *= -1.0;
+        }
+        if rng.gen::<bool>() {
+            y_vel *= -1.0;
+        }
+
+        Ball {
+            rect: Rect::new(
+                SCREEN_WIDTH / 2.0 - BALL_RADIUS / 2.0,
+                SCREEN_HEIGHT / 2.0 - BALL_RADIUS / 2.0,
+                BALL_RADIUS,
+                BALL_RADIUS,
+            ),
+            vel: Vector { x: x_vel, y: y_vel },
+        }
+    }
+}
+
 /*fn draw_rectangle(ctx: &mut ggez::Context, rect: &Rect) -> GameResult {
     use ggez::graphics;
     use ggez::graphics::Color;
@@ -42,6 +72,47 @@ struct Ball {
 
 impl EventHandler for MainState {
     fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
+        if keyboard::is_key_pressed(ctx, keyboard::KeyCode::W) {
+            self.l_paddle.y -= PADDLE_SPEED;
+        }
+        if keyboard::is_key_pressed(ctx, keyboard::KeyCode::S) {
+            self.l_paddle.y += PADDLE_SPEED;
+        }
+        if keyboard::is_key_pressed(ctx, keyboard::KeyCode::Up) {
+            self.r_paddle.y -= PADDLE_SPEED;
+        }
+        if keyboard::is_key_pressed(ctx, keyboard::KeyCode::Down) {
+            self.r_paddle.y += PADDLE_SPEED;
+        }
+
+        self.ball.rect.translate(self.ball.vel);
+
+        // ball paddle collisions
+        if (self.ball.vel.x < 0.0 && self.ball.rect.overlaps(&self.l_paddle))
+            || (self.ball.vel.x > 0.0 && self.ball.rect.overlaps(&self.r_paddle))
+        {
+            self.ball.vel.x *= -1.0;
+        }
+
+        // ball top/bottom collisions
+        if (self.ball.vel.y < 0.0 && self.ball.rect.top() < 0.0)
+            || (self.ball.vel.y > 0.0 && self.ball.rect.bottom() > SCREEN_HEIGHT)
+        {
+            self.ball.vel.y *= -1.0;
+        }
+
+        // score keeping
+        if self.ball.rect.left() < 0.0 {
+            self.r_score += 1;
+            std::thread::sleep(std::time::Duration::from_millis(1000));
+            self.ball = Ball::new();
+        }
+        if self.ball.rect.right() > SCREEN_WIDTH {
+            self.l_score += 1;
+            std::thread::sleep(std::time::Duration::from_millis(1000));
+            self.ball = Ball::new();
+        }
+
         Ok(())
     }
 
@@ -80,6 +151,17 @@ impl EventHandler for MainState {
         graphics::draw(ctx, &r_paddle_mesh, graphics::DrawParam::default())
             .expect("error drawing ball mesh");
 
+        // scoreboard drawing
+        let mut scoreboard_text =
+            graphics::Text::new(format!("L: {} \t R: {}", self.l_score, self.r_score));
+        scoreboard_text.set_font(graphics::Font::default(), graphics::Scale::uniform(24.0));
+        let coords = [
+            SCREEN_WIDTH / 2.0 - scoreboard_text.width(ctx) as f32 / 2.0,
+            10.0,
+        ];
+        let params = graphics::DrawParam::default().dest(coords);
+        graphics::draw(ctx, &scoreboard_text, params).expect("error drawing scoreboard text");
+
         graphics::present(ctx).expect("error presenting");
         Ok(())
     }
@@ -104,15 +186,7 @@ fn main() -> ggez::GameResult {
             PADDLE_WIDTH,
             PADDLE_HEIGHT,
         ),
-        ball: Ball {
-            rect: Rect::new(
-                SCREEN_WIDTH / 2.0 - BALL_RADIUS / 2.0,
-                SCREEN_HEIGHT / 2.0 - BALL_RADIUS / 2.0,
-                BALL_RADIUS,
-                BALL_RADIUS,
-            ),
-            vel: Vector { x: 0.0, y: 0.0 },
-        },
+        ball: Ball::new(),
         l_score: 0,
         r_score: 0,
     };
